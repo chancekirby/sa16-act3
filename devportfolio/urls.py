6ec67c825ff9678280ff92a01cb35b5fc7df0183
Chance Kirby <chance14@comcast.net>
2024-04-25 14:31:32 -0500

    Completed the django portfolio

--- devportfolio/urls.py
+++ devportfolio/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin 
+from django.urls import include, path 
+from django.views.generic.base import RedirectView 
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path("", RedirectView.as_view(url="home/", permanent=True)),
 ]
